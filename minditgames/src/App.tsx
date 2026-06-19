b57c77b9607ccf4b7bdcf853e6755e55e447d5d5
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2026-06-20 01:46:59 +0500

    feat: add base url for minditgames

--- minditgames/src/App.tsx
+++ minditgames/src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ScrollToTop from './components/MindItGames/ScrollToTop.tsx'
-import Home from './pages/Home.tsx'
-import PrivacyPolicy from './pages/PrivacyPolicy.tsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/MindItGames/ScrollToTop.tsx";
+import Home from "./pages/Home.tsx";
+import PrivacyPolicy from "./pages/PrivacyPolicy.tsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/minditgames">
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
